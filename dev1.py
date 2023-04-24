8e42ebf3d2c0ad98b3c00fd4e739bc39b310d51c
felipebns <felipebns@al.insper.edu.br>
2023-04-24 16:04:01 -0300

    define_posições.py

--- dev1.py
+++ dev1.py
@@ -1,1 +1,40 @@
-print('Felipe Bakowski')
+def define_posicoes(linha, coluna, orientacao, tamanho):
+    i = 0
+    retorno = [0,0]*tamanho
+    retorno2 = []
+    grid = [
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0,0,0],
+  ] 
+    if orientacao == 'vertical':
+        retorno = [0,coluna]*tamanho
+        while i < tamanho:
+            retorno = [linha+ i, coluna]
+            retorno2.append(retorno)
+            i += 1
+    elif orientacao == 'horizontal':
+        retorno = [linha,0]*tamanho
+        while i < tamanho:
+            retorno = [linha, coluna + i]
+            retorno2.append(retorno)
+            i += 1
+    return retorno2
+    
+
+
+
+
+
+
+print(define_posicoes(linha = 2,
+coluna = 4,
+orientacao = "vertical",
+tamanho = 3))
