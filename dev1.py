0e39ffe5227948e8690f8dfe81ce0a5e1f0f1cf6
felipebns <131299435+felipebns@users.noreply.github.com>
2023-05-02 17:31:24 -0300

    Update dev1.py

--- dev1.py
+++ dev1.py
@@ -1,36 +1,4 @@
 def define_posicoes(linha, coluna, orientacao, tamanho):
-    i = 0
-    retorno = [0,0]*tamanho
-    retorno2 = []
-    grid = [
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0,0,0],
-  ] 
-    if orientacao == 'vertical':
-        retorno = [0,coluna]*tamanho
-        while i < tamanho:
-            retorno = [linha+ i, coluna]
-            retorno2.append(retorno)
-            i += 1
-    elif orientacao == 'horizontal':
-        retorno = [linha,0]*tamanho
-        while i < tamanho:
-            retorno = [linha, coluna + i]
-            retorno2.append(retorno)
-            i += 1
-    return retorno2
-
-
-
-def define_posicoes(linha, coluna, orientacao, tamanho):
     posicao = []
     if orientacao == 'horizontal':
         for i in range(tamanho):
